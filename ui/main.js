77aea4b9644c1fd0ac0afc1523b40b26cc17e8f2
ksanthoshskireeti <30860005+ksanthoshskireeti@users.noreply.github.com>
2017-08-31 12:12:39 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,21 +1,2 @@
 console.log('Loaded!');
 
-//change the text of the main div
-
-var element = document.getElementById('main-text');
-
-element.innerHTML = 'new value';
-
-// MOVE THE IMAGE  OR ANIMATE
-
-var img = document.getElementById('madi');
-var marginLeft = 0;
-function moveRight(){
-    marginLeft = marginLeft +1;
-    img.style.marginLeft = marginLeft +'px';
-}
-
-img.onclick = function(){
-    var interval = setInterval(moveRight,50);
-    
-};
